b4d8c382cd434aedc5d4d6c851424c814da34d03
Salmaan Ali <salmaan.arif.ali@gmail.com>
2026-01-03 14:18:39 -0500

    set the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,15 @@
 Rails.application.routes.draw do
   resource :session
   resources :passwords, param: :token
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :users, only: [ :new, :create, :update ]
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  resources :flights
+  resources :airports, only: [ :index, :show ]
 
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+  # React
+  get "/app", to: "react_shell#show"
+  get "/app/*path", to: "react_shell#show"
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  # Hotwire
+  root "home#show"
 end
