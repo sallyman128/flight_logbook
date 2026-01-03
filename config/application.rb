ab36a9aab3dc85d20f2259017a40ccac80616edd
Salmaan Ali <salmaan.arif.ali@gmail.com>
2026-01-03 14:27:02 -0500

    switch from minitest w fixtures to rspec w factory

--- config/application.rb
+++ config/application.rb
@@ -16,12 +16,15 @@ module FlightLogbook
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.generators do |g|
+      g.test_framework :rspec, fixtures: true,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false,
+        request_specs: true,
+        controller_specs: false
+
+      g.fixture_replacement :factory_bot, dir: "spec/factories"
+    end
   end
 end
