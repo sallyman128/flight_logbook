6381998646061d0695e65d6a2eeba9fd7c9d089d
Salmaan Ali <salmaan.arif.ali@gmail.com>
2026-01-03 16:32:53 -0500

    flight show

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,10 +1,20 @@
 class FlightsController < ApplicationController
   before_action :require_authentication
+  before_action :get_flight, only: :show
 
   def index
     @flights = Current.user.flights.includes(:departure_airport, :arrival_airport).order(departure_date: :desc, created_at: :desc)
   end
 
   def new
+  end
+
+  def show
+  end
+
+  private
+
+  def get_flight
+    @flight = Flight.find(params[:id])
   end
 end
