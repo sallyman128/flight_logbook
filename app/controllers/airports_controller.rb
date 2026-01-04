00e9e456568dce9b29d466f759c9235bb9ec5728
Salmaan Ali <salmaan.arif.ali@gmail.com>
2026-01-04 14:23:28 -0500

    search filter on airports

--- app/controllers/airports_controller.rb
+++ app/controllers/airports_controller.rb
@@ -2,6 +2,7 @@ class AirportsController < ApplicationController
   before_action :require_authentication
 
   def index
-    @airports = Airport.order(:icao_code).page(params[:page]).per(10)
+    @airport_search_query = params[:airport_search_query].to_s
+    @airports = Airport.search(@airport_search_query).order(:iata_code).page(params[:page]).per(10)
   end
 end
--- app/models/airport.rb
+++ app/models/airport.rb
@@ -1,4 +1,14 @@
 class Airport < ApplicationRecord
   has_many :departing_flights, class_name: "Flight", foreign_key: :departure_airport_id, dependent: :restrict_with_error
   has_many :arriving_flights,  class_name: "Flight", foreign_key: :arrival_airport_id,   dependent: :restrict_with_error
+
+  scope :search, ->(query) do
+    q = query.to_s.strip
+    next all if q.blank?
+
+    where(
+      "iata_code ILIKE :q OR name ILIKE :q OR city ILIKE :q OR region ILIKE :q",
+      q: "%#{sanitize_sql_like(q)}%"
+    )
+  end
 end
