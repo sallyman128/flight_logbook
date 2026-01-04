e5a8dab8798d24b98c9609c53e8f1681586f2b3a
Salmaan Ali <salmaan.arif.ali@gmail.com>
2026-01-04 18:47:18 -0500

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,20 @@ user =
 
 puts "✓ User created: #{user.email_address}"
 
+unless Airport.count > 0
+  puts "Fetching aiports"
+  AirportImporter.import!
+  puts "Fetched #{Airport.count} airports"
+end
+
+iad = Airport.find_by(iata_code: "IAD")
+ord = Airport.find_by(iata_code: "ORD")
+sea = Airport.find_by(iata_code: "SEA")
+lax = Airport.find_by(iata_code: "LAX")
+user.flights.create(departure_date: Date.today, departure_airport: iad, arrival_airport: sea)
+user.flights.create(departure_date: Date.today - 1.year, departure_airport: ord, arrival_airport: lax)
+user.flights.create(departure_date: Date.today - 2.years, departure_airport: lax, arrival_airport: sea)
+
 puts "Seeding complete ✅"
 puts
 puts "Login with:"
